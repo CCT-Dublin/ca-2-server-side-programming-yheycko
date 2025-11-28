202bc5e2cf860ec30c303d658179dc21721de746
YeisonQuinteros2024261 <yheycko@gmail.com>
2025-11-28 01:13:50 +0000

    Here in this part we make all to get conexion and if somenthing not working they give you a message which gonna be Error to connect

--- database.js
+++ database.js
@@ -1,7 +1,17 @@
 const mysql = require('mysql2');
 const connection =mysql.createConnection({
 host: 'localhost',
-user: 'Yeison',
+user: 'Yheycko-CCT',
 password: 'Yheison123@',
 database: 'Server_ca2'
-});    
+}); 
+
+connection.connect((err) => {
+if (err) {
+console.error('Error when you connect the database:',err);
+} else {
+console.log('successful conexion in database');
+}
+});
+
+MediaSourceHandle.exports = connection;    
